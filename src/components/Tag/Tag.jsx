ef5a81778b4f9033b2ddf842da9c4c700242041f
Behzad Sharafi <behzadsharafi@gmail.com>
2023-12-28 21:27:51 +1100

    test

--- src/components/Tag/Tag.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Tag.module.scss";
-
-const Tag = ({ children, className }) => {
-  return <span className={`${styles.tag} ${className}`}>{children}</span>;
-};
-
-export default Tag;
--- src/containers/LogoContainer/LogoContainer.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./LogoContainer.module.scss";
-
-const LogoContainer = () => {
-  return <div className={styles.canvas}>LogoContainer</div>;
-};
-
-export default LogoContainer;
